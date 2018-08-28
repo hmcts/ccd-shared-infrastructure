95458e5ca0b37c2750c089c7087c4c38e25114d9
Dwayne Bailey <Dwayne.Bailey@HMCTS.net>
2018-08-28 11:40:15 +0100

    Adjust 'tags' to expected shared-infra tags
    See
    https://tools.hmcts.net/confluence/display/CNP/Shared+Infrastructure+Repository+DRAFT

--- main.tf
+++ main.tf
@@ -1,4 +1,12 @@
+// Shared Resource Group
 resource "azurerm_resource_group" "rg" {
   name     = "${var.product}-shared-${var.env}"
   location = "${var.location}"
+
+  tags {
+    "Deployment Environment" = "${var.env}"
+    "Team Name" = "${var.team_name}"
+    "Team Contact" = "${var.team_contact}"
+    "Destroy Me" = "${var.destroy_me}"
+  }
 }
