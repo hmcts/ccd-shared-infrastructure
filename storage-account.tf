c7ea9f44356f5dc98ebaa33648f2ecf8a55e82e1
Dwayne Bailey <Dwayne.Bailey@HMCTS.net>
2018-08-30 09:24:40 +0100

    Storage: use the global resource group

--- storage-account.tf
+++ storage-account.tf
@@ -1,6 +1,6 @@
 resource "azurerm_storage_account" "storage" {
   name = "${var.product}store${var.env}"
-  resource_group_name = "${var.product}-shared-${var.env}"
+  resource_group_name = "${azurerm_resource_group.rg.name}"
   location = "${var.location}"
   account_tier = "Standard"
   account_replication_type = "LRS"
