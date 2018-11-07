8cb74261b96b8d9651df440ffc193c028e90b6ea
Dwayne Bailey <Dwayne.Bailey@HMCTS.net>
2018-11-07 13:11:46 +0000

    Use versioned storage account module
    RDM-3316

--- storage-account.tf
+++ storage-account.tf
@@ -3,7 +3,7 @@
 
 // Shared Storage Account
 module "storage_account" {
-  source                    = "git@github.com:hmcts/cnp-module-storage-account.git?ref=master"
+  source                    = "git@github.com:hmcts/cnp-module-storage-account.git?ref=0.0.1"
   env                       = "${var.env}"
   storage_account_name      = "${var.product}shared${var.env}"
   resource_group_name       = "${azurerm_resource_group.rg.name}"
@@ -63,7 +63,7 @@ output "storage_account_name" {
 
 // dm-store blob Storage Account
 module "dm_store_storage_account" {
-  source                    = "git@github.com:hmcts/cnp-module-storage-account.git?ref=master"
+  source                    = "git@github.com:hmcts/cnp-module-storage-account.git?ref=0.0.1"
   env                       = "${var.env}"
   storage_account_name      = "dmstoredoc${var.env}"
   resource_group_name       = "${azurerm_resource_group.rg.name}"
