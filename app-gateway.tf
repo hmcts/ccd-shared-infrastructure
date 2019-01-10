3afb0c9b4d1e65fdea5425ff8c741fe86c8888ab
tzarmango <fatih.ozceylan@gmail.com>
2019-01-10 14:24:03 +0000

    RDM-3272 pahBasedRule support of Waf being tested - pathRules moved to another section

--- app-gateway.tf
+++ app-gateway.tf
@@ -183,7 +183,7 @@ module "appGw" {
   pathRules = [
     {
       name                        = "https-url-path-map-gateway-rule-palo-alto"
-      paths                       = ["/documents"]
+      paths                       = "/documents"
       backendAddressPool          = "${var.product}-${var.env}-palo-alto"
       backendHttpSettings         = "backend-443-nocookies-gateway"
     }
