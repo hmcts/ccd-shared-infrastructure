9153f2238c3e5367ad1c6791d9236d193df12b75
Dwayne Bailey <Dwayne.Bailey@HMCTS.net>
2019-04-29 12:29:32 +0100

    Use key_vault_id for cert
    RDM-4236

--- app-gateway.tf
+++ app-gateway.tf
@@ -1,6 +1,11 @@
+data "azurerm_key_vault" "infra_vault" {
+  name = "infra-vault-${var.subscription}"
+  resource_group_name = "${var.subscription == "prod" ? "core-infra-prod" : "cnp-core-infra"}"
+}
+
 data "azurerm_key_vault_secret" "cert" {
   name      = "${var.external_cert_name}"
-  vault_uri = "https://infra-vault-${var.subscription}.vault.azure.net/"
+  key_vault_id = "${data.azurerm_key_vault.infra_vault.id}"
 }
 
 
