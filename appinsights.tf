ca0c2c1ebd0a03ab40ee73c75b75055cd444398d
mutlu <mutlu@localhost>
2019-06-07 15:20:05 +0100

    add web pages ai resource

--- appinsights.tf
+++ appinsights.tf
@@ -8,3 +8,16 @@ resource "azurerm_application_insights" "appinsights" {
 output "appInsightsInstrumentationKey" {
   value = "${azurerm_application_insights.appinsights.instrumentation_key}"
 }
+
+resource "azurerm_key_vault_secret" "app_insights_key" {
+  name         = "AppInsightsInstrumentationKey"
+  value        = "${azurerm_application_insights.appinsights.instrumentation_key}"
+  key_vault_id = "${module.vault.key_vault_id}"
+}
+
+resource "azurerm_application_insights" "appinsights_webpages" {
+  name                = "${var.product}-webpages-${var.env}"
+  location            = "${var.location}"
+  resource_group_name = "${azurerm_resource_group.rg.name}"
+  application_type    = "${var.application_type}"
+}
