7516dd7305415e91886a3a9534dd485017e1a4d6
nathan-clark <56871207+nathan-clark@users.noreply.github.com>
2020-02-07 14:02:20 +0000

    RDM-7000-perftest

--- app-gateway.tf
+++ app-gateway.tf
@@ -119,7 +119,7 @@ module "appGw" {
       probe                          = "http-probe-gateway"
       PickHostNameFromBackendAddress = "False"
       HostName                       = "${var.external_hostname_gateway}"
-      timeout                        = "${var.documents_request_timeout}"
+      requestTimeout                 = 300
     },
     {
       name                           = "backend-80-nocookies-www"
