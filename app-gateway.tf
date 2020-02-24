67c9f8a320f451a133f365829eb7a19fb19703ca
Neil Richardson <960806+njrich28@users.noreply.github.com>
2020-02-24 11:36:32 +0000

    Bump requestTimeout
    Fix for property existence check applied to module

--- app-gateway.tf
+++ app-gateway.tf
@@ -119,7 +119,7 @@ module "appGw" {
       probe                          = "http-probe-gateway"
       PickHostNameFromBackendAddress = "False"
       HostName                       = "${var.external_hostname_gateway}"
-      requestTimeout                 = 400
+      requestTimeout                 = 301
     },
     {
       name                           = "backend-80-nocookies-www"
