62ef9bb2b33b8fbd38a189ee1d615a5e104b5763
nathan-clark <56871207+nathan-clark@users.noreply.github.com>
2020-05-14 10:50:16 +0100

    RDM-2703

--- alert_action_group.tf
+++ alert_action_group.tf
@@ -1,7 +1,3 @@
-data "azurerm_key_vault_secret" "alert_ccd_email_secret" {
-  name      = "ccd-alert-email"
-  vault_uri = "${module.vault.key_vault_uri}"
-}
 
 module "alert-action-group" {
   source   = "git@github.com:hmcts/cnp-module-action-group"
@@ -13,12 +9,6 @@ module "alert-action-group" {
   short_name             = "ccd_alerts"
   email_receiver_name    = "CCD Alerts And Monitoring"
   email_receiver_address = "${data.azurerm_key_vault_secret.alert_ccd_email_secret.value}"
-}
-
-resource "azurerm_key_vault_secret" "alert_action_group_name" {
-  name = "alert-action-group-name"
-  value = "${module.alert-action-group.action_group_name}"
-  vault_uri = "${data.azurerm_key_vault.key_vault.vault_uri}"
 }
 
 output "action_group_name" {
