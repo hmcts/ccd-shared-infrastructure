4921b1c536f420611a1b01fd44f22962ccb6ba00
nathan-clark <56871207+nathan-clark@users.noreply.github.com>
2020-09-29 16:15:41 +0100

    Update appinsights.tf

--- appinsights.tf
+++ appinsights.tf
@@ -3,6 +3,14 @@ resource "azurerm_application_insights" "appinsights" {
   location            = var.location
   resource_group_name = azurerm_resource_group.rg.name
   application_type    = var.application_type
+  
+   lifecycle {
+    ignore_changes = [
+      # Ignore changes to appinsights as otherwise upgrading to the Azure provider 2.x
+      # destroys and re-creates this appinsights instance
+      application_type,
+    ]
+  }
 }
 
 output "appInsightsInstrumentationKey" {
