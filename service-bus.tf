ed1a28f9257d5e7a159b174fdcaf136979abf334
nathan-clark <56871207+nathan-clark@users.noreply.github.com>
2020-11-04 14:24:03 +0000

    Create service-bus.tf

---
+++ service-bus.tf
@@ -0,0 +1,22 @@
+locals {
+  events_topic_name         = "${var.product}-events-topic-${var.env}"
+  servicebus_namespace_name = "${var.product}-servicebus-${var.env}"
+  resource_group_name       = azurerm_resource_group.rg.name
+}
+
+module "servicebus-namespace" {
+  source              = "git@github.com:hmcts/terraform-module-servicebus-namespace?ref=master"
+  name                = local.servicebus_namespace_name
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+  env                 = var.env
+  common_tags         = local.tags
+  sku                 = "Premium"
+}
+
+module "events-topic" {
+  source              = "git@github.com:hmcts/terraform-module-servicebus-topic?ref=master"
+  name                = local.events_topic_name 
+  namespace_name      = local.servicebus_namespace_name
+  resource_group_name = local.resource_group_name
+}
