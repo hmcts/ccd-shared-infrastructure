f234323ad87170bcba3cc8b2edb6ceecda296777
nathan-clark <56871207+nathan-clark@users.noreply.github.com>
2020-11-05 09:10:46 +0000

    Update service-bus.tf

--- service-bus.tf
+++ service-bus.tf
@@ -5,13 +5,15 @@ locals {
 }
 
 module "servicebus-namespace" {
-  source              = "git@github.com:hmcts/terraform-module-servicebus-namespace?ref=master"
+#   source              = "git@github.com:hmcts/terraform-module-servicebus-namespace?ref=master"
+  source              = "git@github.com:nathan-clark/terraform-module-servicebus-namespace?ref=patch-2"
   name                = local.servicebus_namespace_name
   location            = var.location
   resource_group_name = azurerm_resource_group.rg.name
   env                 = var.env
   common_tags         = local.tags
   sku                 = "Premium"
+  zoneRedundant       = true
 }
 
 module "events-topic" {
