d68dec21550fab5d448b2c84d6cd0e8245581ab4
nathan-clark <56871207+nathan-clark@users.noreply.github.com>
2020-11-17 10:50:11 +0000

    Update storage-account.tf

--- storage-account.tf
+++ storage-account.tf
@@ -3,7 +3,7 @@
 
 // Shared Storage Account
 module "storage_account" {
-  source                    = "git@github.com:hmcts/cnp-module-storage-account.git?ref=master"
+  source                    = "git@github.com:hmcts/cnp-module-storage-account?ref=master"
   env                       = "${var.env}"
   storage_account_name      = "${var.product}shared${var.env}"
   resource_group_name       = "${azurerm_resource_group.rg.name}"
@@ -68,7 +68,7 @@ output "storage_account_name" {
 
 // dm-store blob Storage Account
 module "dm_store_storage_account" {
-  source                    = "git@github.com:hmcts/cnp-module-storage-account.git?ref=master"
+  source                    = "git@github.com:hmcts/cnp-module-storage-account?ref=master"
   env                       = "${var.env}"
   storage_account_name      = "dmstoredoc${var.env}"
   resource_group_name       = "${azurerm_resource_group.rg.name}"
