216085d1c2e2df97dfe58cbcbf8d9d6feca97f24
nathan-clark <56871207+nathan-clark@users.noreply.github.com>
2020-11-17 09:59:25 +0000

    Update key-vault.tf

--- key-vault.tf
+++ key-vault.tf
@@ -1,5 +1,5 @@
 module "vault" {
-  source = "git@github.com:hmcts/cnp-module-key-vault?ref=azurermv2"
+  source = "git@github.com:hmcts/cnp-module-key-vault?ref=master"
   name = "ccd-${var.env}"
   product = "${var.product}"
   env = "${var.env}"
