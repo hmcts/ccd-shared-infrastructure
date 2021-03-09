a4480a86dc910c270ee8fa88de16de6bcf498209
TomKimber <thomas.kimber@hmcts.net>
2021-03-09 12:41:05 +0000

    Linted storage-account.tf.

--- storage-account.tf
+++ storage-account.tf
@@ -3,15 +3,15 @@
 
 // Shared Storage Account
 module "storage_account" {
-  source                    = "git@github.com:hmcts/cnp-module-storage-account?ref=master"
-  env                       = "${var.env}"
-  storage_account_name      = "${var.product}shared${var.env}"
-  resource_group_name       = "${azurerm_resource_group.rg.name}"
-  location                  = "${var.location}"
-  account_kind              = "StorageV2"
-  account_tier              = "Standard"
-  account_replication_type  = "LRS"
-  access_tier               = "Hot"
+  source                   = "git@github.com:hmcts/cnp-module-storage-account?ref=master"
+  env                      = "${var.env}"
+  storage_account_name     = "${var.product}shared${var.env}"
+  resource_group_name      = "${azurerm_resource_group.rg.name}"
+  location                 = "${var.location}"
+  account_kind             = "StorageV2"
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  access_tier              = "Hot"
 
   enable_https_traffic_only = true
 
@@ -28,37 +28,37 @@ module "storage_account" {
 
 // Storage Account Vault Secrets
 resource "azurerm_key_vault_secret" "storageaccount_id" {
-  depends_on = ["module.vault"]
-  name      = "storage-account-id"
-  value     = "${module.storage_account.storageaccount_id}"
+  depends_on   = ["module.vault"]
+  name         = "storage-account-id"
+  value        = "${module.storage_account.storageaccount_id}"
   key_vault_id = "${module.vault.key_vault_id}"
 }
 
 resource "azurerm_key_vault_secret" "storageaccount_primary_access_key" {
-  depends_on = ["module.vault"]
-  name      = "storage-account-primary-access-key"
-  value     = "${module.storage_account.storageaccount_primary_access_key}"
+  depends_on   = ["module.vault"]
+  name         = "storage-account-primary-access-key"
+  value        = "${module.storage_account.storageaccount_primary_access_key}"
   key_vault_id = "${module.vault.key_vault_id}"
 }
 
 resource "azurerm_key_vault_secret" "storageaccount_secondary_access_key" {
-  depends_on = ["module.vault"]
-  name      = "storage-account-secondary-access-key"
-  value     = "${module.storage_account.storageaccount_secondary_access_key}"
+  depends_on   = ["module.vault"]
+  name         = "storage-account-secondary-access-key"
+  value        = "${module.storage_account.storageaccount_secondary_access_key}"
   key_vault_id = "${module.vault.key_vault_id}"
 }
 
 resource "azurerm_key_vault_secret" "storageaccount_primary_connection_string" {
-  depends_on = ["module.vault"]
-  name      = "storage-account-primary-connection-string"
-  value     = "${module.storage_account.storageaccount_primary_connection_string}"
+  depends_on   = ["module.vault"]
+  name         = "storage-account-primary-connection-string"
+  value        = "${module.storage_account.storageaccount_primary_connection_string}"
   key_vault_id = "${module.vault.key_vault_id}"
 }
 
 resource "azurerm_key_vault_secret" "storageaccount_secondary_connection_string" {
-  depends_on = ["module.vault"]
-  name      = "storage-account-secondary-connection-string"
-  value     = "${module.storage_account.storageaccount_secondary_connection_string}"
+  depends_on   = ["module.vault"]
+  name         = "storage-account-secondary-connection-string"
+  value        = "${module.storage_account.storageaccount_secondary_connection_string}"
   key_vault_id = "${module.vault.key_vault_id}"
 }
 
@@ -70,15 +70,15 @@ output "storage_account_name" {
 
 // dm-store blob Storage Account
 module "dm_store_storage_account" {
-  source                    = "git@github.com:hmcts/cnp-module-storage-account?ref=master"
-  env                       = "${var.env}"
-  storage_account_name      = "dmstoredoc${var.env}"
-  resource_group_name       = "${azurerm_resource_group.rg.name}"
-  location                  = "${var.location}"
-  account_kind              = "StorageV2"
-  account_tier              = "Standard"
-  account_replication_type  = "LRS"
-  access_tier               = "Hot"
+  source                   = "git@github.com:hmcts/cnp-module-storage-account?ref=master"
+  env                      = "${var.env}"
+  storage_account_name     = "dmstoredoc${var.env}"
+  resource_group_name      = "${azurerm_resource_group.rg.name}"
+  location                 = "${var.location}"
+  account_kind             = "StorageV2"
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  access_tier              = "Hot"
 
   enable_https_traffic_only = true
 
@@ -95,37 +95,37 @@ module "dm_store_storage_account" {
 
 // dm-store blob Storage Account Vault Secrets
 resource "azurerm_key_vault_secret" "dm_store_storageaccount_id" {
-  depends_on = ["module.vault"]
-  name      = "dm-store-storage-account-id"
-  value     = "${module.dm_store_storage_account.storageaccount_id}"
+  depends_on   = ["module.vault"]
+  name         = "dm-store-storage-account-id"
+  value        = "${module.dm_store_storage_account.storageaccount_id}"
   key_vault_id = "${module.vault.key_vault_id}"
 }
 
 resource "azurerm_key_vault_secret" "dm_store_storageaccount_primary_access_key" {
-  depends_on = ["module.vault"]
-  name      = "dm-store-storage-account-primary-access-key"
-  value     = "${module.dm_store_storage_account.storageaccount_primary_access_key}"
+  depends_on   = ["module.vault"]
+  name         = "dm-store-storage-account-primary-access-key"
+  value        = "${module.dm_store_storage_account.storageaccount_primary_access_key}"
   key_vault_id = "${module.vault.key_vault_id}"
 }
 
 resource "azurerm_key_vault_secret" "dm_store_storageaccount_secondary_access_key" {
-  depends_on = ["module.vault"]
-  name      = "dm-store-storage-account-secondary-access-key"
-  value     = "${module.dm_store_storage_account.storageaccount_secondary_access_key}"
+  depends_on   = ["module.vault"]
+  name         = "dm-store-storage-account-secondary-access-key"
+  value        = "${module.dm_store_storage_account.storageaccount_secondary_access_key}"
   key_vault_id = "${module.vault.key_vault_id}"
 }
 
 resource "azurerm_key_vault_secret" "dm_store_storageaccount_primary_connection_string" {
-  depends_on = ["module.vault"]
-  name      = "dm-store-storage-account-primary-connection-string"
-  value     = "${module.dm_store_storage_account.storageaccount_primary_connection_string}"
+  depends_on   = ["module.vault"]
+  name         = "dm-store-storage-account-primary-connection-string"
+  value        = "${module.dm_store_storage_account.storageaccount_primary_connection_string}"
   key_vault_id = "${module.vault.key_vault_id}"
 }
 
 resource "azurerm_key_vault_secret" "dm_store_storageaccount_secondary_connection_string" {
-  depends_on = ["module.vault"]
-  name      = "dm-store-storage-account-secondary-connection-string"
-  value     = "${module.dm_store_storage_account.storageaccount_secondary_connection_string}"
+  depends_on   = ["module.vault"]
+  name         = "dm-store-storage-account-secondary-connection-string"
+  value        = "${module.dm_store_storage_account.storageaccount_secondary_connection_string}"
   key_vault_id = "${module.vault.key_vault_id}"
 }
 
