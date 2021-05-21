4a4bdda5168e2669bd3c104e61d2a9fdb2f8ba2b
nathan-clark <56871207+nathan-clark@users.noreply.github.com>
2021-05-21 12:40:57 +0100

    Update state.tf

--- state.tf
+++ state.tf
@@ -2,6 +2,25 @@ provider "azurerm" {
   features {}
 }
 
+provider "azurerm" {
+  alias           = "aks-infra"
+  subscription_id = "${var.aks_infra_subscription_id}"
+  features {}
+  skip_provider_registration = true
+}
+
+provider "azurerm" {
+  alias           = "aks-preview"
+  subscription_id = "${var.aks_preview_subscription_id}"
+  features {}
+}
+
+provider "azurerm" {
+  alias           = "mgmt"
+  subscription_id = "${var.mgmt_subscription_id}"
+  features {}
+}
+
 terraform {
   required_version = ">= 0.13"  # Terraform client version
 
