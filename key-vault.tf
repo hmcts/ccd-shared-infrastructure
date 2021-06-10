742b418364b10d992b4bbcfd94f43b010c57d58d
Sateesh Mathangi <sateeshkumar.m@gmail.com>
2021-06-10 13:56:26 +0100

    fixed product AD group dcd_ccd

--- key-vault.tf
+++ key-vault.tf
@@ -6,7 +6,7 @@ module "vault" {
   tenant_id = "${var.tenant_id}"
   object_id = "${var.jenkins_AAD_objectId}"
   resource_group_name = "${azurerm_resource_group.rg.name}"
-  product_group_object_id = "be8b3850-998a-4a66-8578-da268b8abd6b"
+  product_group_name = "dcd_ccd"  
 
   common_tags = "${local.tags}"
 
