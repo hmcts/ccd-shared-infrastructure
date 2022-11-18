d7865a427fc620724c80e46d05759d6901f74364
yogesh-hullatti <53007108+yogesh-hullatti@users.noreply.github.com>
2022-11-18 15:05:46 +0000

    Added in changes for Dm-Store tag (#143)
    * Added in changes for Dm-Store tag
    
    * Updated with review comment

--- main.tf
+++ main.tf
@@ -6,6 +6,17 @@ locals {
       "Destroy Me" = var.destroy_me
     })
   )
+  em_tags = merge(
+      var.common_tags,
+      tomap({
+        "Team Contact"        = var.em_team_contact
+        "Destroy Me"          = var.em_destroy_me
+        "application"         = "evidence-management"
+        "managedBy"           = "Evidence Management"
+        "businessArea"        = "CFT"
+        "contactSlackChannel" = var.em_team_contact
+      })
+  )
 }
 
 // Shared Resource Group
