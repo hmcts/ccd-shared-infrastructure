5321b868a4b33cd9f9953e83b042e3a7e56940c0
58046 <62422075+hmcts-jenkins-a-to-c[bot]@users.noreply.github.com>
2024-06-28 06:34:29 +0000

    Updating Terraform Formatting

--- main.tf
+++ main.tf
@@ -3,19 +3,19 @@ locals {
     var.common_tags,
     tomap({
       "Team Contact" = var.team_contact
-      "Destroy Me" = var.destroy_me
+      "Destroy Me"   = var.destroy_me
     })
   )
   em_tags = merge(
-      var.common_tags,
-      tomap({
-        "Team Contact"        = var.em_team_contact
-        "Destroy Me"          = var.em_destroy_me
-        "application"         = "evidence-management"
-        "managedBy"           = "Evidence Management"
-        "businessArea"        = "CFT"
-        "contactSlackChannel" = var.em_team_contact
-      })
+    var.common_tags,
+    tomap({
+      "Team Contact"        = var.em_team_contact
+      "Destroy Me"          = var.em_destroy_me
+      "application"         = "evidence-management"
+      "managedBy"           = "Evidence Management"
+      "businessArea"        = "CFT"
+      "contactSlackChannel" = var.em_team_contact
+    })
   )
 }
 
