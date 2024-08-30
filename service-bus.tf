39243b794f1ac19fc990554379475294e087e0ba
Dan Lysiak <50049163+danlysiak@users.noreply.github.com>
2024-08-30 09:39:23 +0100

    Update service-bus.tf

--- service-bus.tf
+++ service-bus.tf
@@ -15,7 +15,6 @@ module "servicebus-namespace" {
   env                 = var.env
   common_tags         = local.tags
   sku                 = var.sku
-  zone_redundant      = var.sku != "Premium" ? "false" : "true"
 }
 
 module "events-topic" {
